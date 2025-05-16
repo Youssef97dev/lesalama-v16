dca69881a65e752d3cb213f6668239e11a85f6c8
Youssef97dev <youssefmoussafi97@gmail.com>
2025-05-16 15:54:53 +0100

    change map

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,13 +16,13 @@ const Contact = () => {
       id="contact"
       className="w-full h-full flex md:flex-row flex-col-reverse"
     >
-      <div className="relative w-full">
+      <div className="relative w-full flex justify-center items-center">
         <Image
           src="https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/Salama/l2si8ubntyyvkmzpo9vk"
           alt="map le salama"
           height={1000}
           width={1000}
-          className="object-cover w-1/2 h-1/2 md:block hidden"
+          className="object-cover w-1/2 md:block hidden"
         />
         <Image
           src="https://res.cloudinary.com/dz7wroord/image/upload/f_auto,q_auto/v1/Salama/l2si8ubntyyvkmzpo9vk"
